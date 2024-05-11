3d8ab0c5172c60f72c3539a2d0c255fc1c8cefd3
Leonid Medovyy <leonid@macron1.io>
2024-05-11 13:39:19 -0700

    modified the dying logic

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,8 +4,11 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You died!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1.0
