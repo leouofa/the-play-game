7226c56da92d98ac89856ebbbc8dbd02cb8ec163
Leonid Medovyy <leonid@macron1.io>
2024-05-12 16:43:20 -0700

    moved score to the global variable

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var score = 0
+#var score = 0
 
 @onready var score_label = $ScoreLabel
 
 func add_point():
-	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+	Autoload.score += 1
+	score_label.text = "You collected " + str(Autoload.score) + " coins."
