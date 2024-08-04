2e62d0216deae63853452c4b62b21025b3561811
Leonid Medovyy <leonid@macron1.io>
2024-08-04 14:19:40 -0700

    setting up the level selector

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,7 +1,10 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
+
+func _on_level_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/levels.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
