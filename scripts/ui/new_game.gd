df8ef7b432fe22346c5cbad7836a7960c21c5985
Leonid Medovyy <leonid@macron1.io>
2024-08-06 12:29:53 -0700

    adding ability to save files

--- scripts/ui/new_game.gd
+++ scripts/ui/new_game.gd
@@ -2,6 +2,23 @@ extends Control
 
 const SAVE_PATH = "user://save_slot_"
 
+var slot1
+var slot2
+var slot3
+
+func _ready():
+	slot1 = $VBoxContainer/SlotOne
+	slot2 = $VBoxContainer/SlotTwo
+	slot3 = $VBoxContainer/SlotThree
+
+	for i in range(1, 4):  # range(start, end) where end is exclusive
+		var slot_name = "slot" + str(i)  # Create the variable name string
+		var slot_variable = get(slot_name)
+
+		if slot_exists(i):
+			slot_variable.set_text("Exists")
+		else:
+			slot_variable.set_text("Empty")
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
@@ -12,15 +29,21 @@ func _on_slot_one_pressed():
 
 
 func _on_slot_two_pressed():
-	pass # Replace with function body.
+	save(2)
 
 
 func _on_slot_three_pressed():
-	pass # Replace with function body.
+	save(3)
 
 func save(save_slot):
 	var file = FileAccess.open(SAVE_PATH + str(save_slot) + ".save", FileAccess.WRITE)
-	var variable1 = 0
-	file.store_var(variable1)
+	var slot_name = "Slot " + str(save_slot)
+	file.store_var(slot_name)
+	file.close()
+
+func slot_exists(save_slot):
+	var file_path = SAVE_PATH + str(save_slot) + ".save"
+	return FileAccess.file_exists(file_path)
+	
 
 		
