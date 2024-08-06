f02e2b9fd2b7c4edd8347c509fae479373c353d9
Leonid Medovyy <leonid@macron1.io>
2024-08-05 18:18:56 -0700

    setting up the pause menu

---
+++ scripts/ui/pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+
+
+func _process(delta):
+	testEsc()
+
+
+func _on_resume_button_pressed():
+	resume()
+
+
+func _on_restart_button_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
