e39c2d4cb64456b50312aa7b40487cda7aee7091
Leonid Medovyy <leonid@macron1.io>
2024-08-06 21:55:59 -0700

    connecting the save data

--- scripts/ui/new_game.gd
+++ scripts/ui/new_game.gd
@@ -5,6 +5,7 @@ const SAVE_PATH = "user://save_slot_"
 var slot1
 var slot2
 var slot3
+var username
 
 func _ready():
 	slot1 = $VBoxContainer/SlotOne
@@ -16,6 +17,11 @@ func _ready():
 		var slot_variable = get(slot_name)
 
 		if slot_exists(i):
+			load_data(i)
+			# var save_file = load_data(i)
+			# username = save_file.get_var(username)
+			# print(save_file.get_var(username))
+
 			slot_variable.set_text("Exists")
 		else:
 			slot_variable.set_text("Empty")
@@ -27,15 +33,18 @@ func _on_back_button_pressed():
 func _on_slot_one_pressed():
 	Autoload.slot = 1
 	get_tree().change_scene_to_file("res://scenes/ui/player_name.tscn")
-	# save(1)
 
 
 func _on_slot_two_pressed():
-	save(2)
+	Autoload.slot = 2
+	get_tree().change_scene_to_file("res://scenes/ui/player_name.tscn")
 
 
 func _on_slot_three_pressed():
-	save(3)
+	Autoload.slot = 3
+	get_tree().change_scene_to_file("res://scenes/ui/player_name.tscn")
+
+	# save(3)
 
 func save(save_slot):
 	var file = FileAccess.open(SAVE_PATH + str(save_slot) + ".save", FileAccess.WRITE)
@@ -43,9 +52,18 @@ func save(save_slot):
 	file.store_var(slot_name)
 	file.close()
 
+func load_data(save_slot):
+	if slot_exists(save_slot):
+		var file_path = SAVE_PATH + str(save_slot) + ".save"
+		var savefile = FileAccess.open(file_path, FileAccess.READ)
+		print(savefile.get_var(true))
+		return savefile
+
+
 func slot_exists(save_slot):
 	var file_path = SAVE_PATH + str(save_slot) + ".save"
 	return FileAccess.file_exists(file_path)
 	
+
 
 		
--- scripts/ui/player_name.gd
+++ scripts/ui/player_name.gd
@@ -2,11 +2,13 @@ extends Control
 
 const SAVE_PATH = "user://save_slot_"
 var warning_label
-var warning_text 
+var warning_text
+var name_input
 
 func _ready():
 	warning_label = $VBoxContainer/WarningLabel
 	warning_text = $VBoxContainer/WarningText
+	name_input = $VBoxContainer/NameInput
 
 	if slot_exists(Autoload.slot):
 		warning_label.visible = true
@@ -20,3 +22,14 @@ func _on_back_button_pressed():
 func slot_exists(save_slot):
 	var file_path = SAVE_PATH + str(save_slot) + ".save"
 	return FileAccess.file_exists(file_path)
+
+
+func _on_new_game_button_pressed():
+	# print(username.text)
+	save(Autoload.slot)
+
+func save(save_slot):
+	var file = FileAccess.open(SAVE_PATH + str(save_slot) + ".save", FileAccess.WRITE)
+	var username = name_input.text
+	file.store_var(username)
+	file.close()
