67c20bacca5b87a213f21f9322c52fdc114c44cb
Leonid Medovyy <leonid@macron1.io>
2024-08-07 14:03:16 -0700

    refactoring the new game screen

--- scripts/ui/new_game.gd
+++ scripts/ui/new_game.gd
@@ -2,15 +2,12 @@ extends Control
 
 const SAVE_PATH = "user://save_slot_"
 
-var slot1
-var slot2
-var slot3
+@onready var	slot1 = $VBoxContainer/SlotOne
+@onready var	slot2 = $VBoxContainer/SlotTwo
+@onready var	slot3 = $VBoxContainer/SlotThree
 var game_data = {}
 
 func _ready():
-	slot1 = $VBoxContainer/SlotOne
-	slot2 = $VBoxContainer/SlotTwo
-	slot3 = $VBoxContainer/SlotThree
 
 	for i in range(1, 4):  # range(start, end) where end is exclusive
 		var slot_name = "slot" + str(i)
@@ -39,14 +36,6 @@ func _on_slot_two_pressed():
 func _on_slot_three_pressed():
 	Autoload.slot = 3
 	get_tree().change_scene_to_file("res://scenes/ui/player_name.tscn")
-
-	# save(3)
-
-func save(save_slot):
-	var file = FileAccess.open(SAVE_PATH + str(save_slot) + ".save", FileAccess.WRITE)
-	var slot_name = "Slot " + str(save_slot)
-	file.store_var(slot_name)
-	file.close()
 
 func load_data(save_slot):
 	if slot_exists(save_slot):
