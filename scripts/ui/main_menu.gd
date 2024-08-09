50547a8ed2ce539c00e960694a24a397fd5cc0d0
Leonid Medovyy <leonid@macron1.io>
2024-08-09 09:52:01 -0700

    adding high scores scene and refactoring main_menu

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,15 +1,26 @@
 extends Control
+
+const SCENE_PATH = "res://scenes/ui/"
+
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 	
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/new_game.tscn")
+	goto_scene("new_game")
 
 func _on_level_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/levels.tscn")
+	goto_scene("levels")
+
+func _on_continue_button_pressed():
+	goto_scene("continue_game")
+
+func _on_high_score_button_pressed():
+	goto_scene("high_scores")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
 
-func _on_continue_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/ui/continue_game.tscn")
+func goto_scene(scene):
+	var scene_file = SCENE_PATH + scene + ".tscn"
+	get_tree().change_scene_to_file(scene_file)
+
