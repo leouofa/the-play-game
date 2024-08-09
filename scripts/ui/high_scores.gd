ba0b51206729a96c2b2afbc1ede31882eba5fecc
Leonid Medovyy <leonid@macron1.io>
2024-08-09 11:12:25 -0700

    setting up the high score calculationg

--- scripts/ui/high_scores.gd
+++ scripts/ui/high_scores.gd
@@ -1,11 +1,46 @@
-extends Control
+extends "res://scripts/ui/base.gd"
 
+@onready var back_button = $VBoxContainer3/BackButton
 
-# Called when the node enters the scene tree for the first time.
+var files = {}
+
 func _ready():
-	pass # Replace with function body.
+	back_button.grab_focus()
 
+	for i in range(1, 4):  # range(start, end) where end is exclusive
+		if slot_exists(i):
+			load_data(i)
+			files[i] = game_data
+	
+	var sorted_scores = get_sorted_usernames_by_score(files)
+	
+	for entry in sorted_scores:
+		print("Username: %s, Total Score: %d" % [entry[0], entry[1]])
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_back_button_pressed():
+	goto_ui_scene("main_menu")
+	
+# Function to calculate total scores and return sorted list
+func get_sorted_usernames_by_score(data):
+	var totals = {}
+	
+	# Calculate total scores for each user
+	for user_id in data.keys():
+		var user = data[user_id]
+		var total_score = 0
+		for score in user["scores"].values():
+			total_score += score
+		totals[user["username"]] = total_score
+	
+	# Convert dictionary to array of tuples and sort
+	var sorted_totals = []
+	for username in totals.keys():
+		sorted_totals.append([username, totals[username]])
+	
+	sorted_totals.sort_custom(_sort_by_score) # Sort in descending order
+	
+	return sorted_totals
+
+# Helper function to sort by score in descending order
+func _sort_by_score(a, b):
+	return b[1] - a[1]
