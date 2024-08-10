0ec2e72c8e79328602dacc02a8240a341106a64f
Leonid Medovyy <leonid@macron1.io>
2024-08-10 13:48:29 -0700

    refactoring the music script

--- scripts/music.gd
+++ scripts/music.gd
@@ -3,20 +3,18 @@ extends Node
 const MUSIC_PATH = "res://assets/music/"
 
 @onready var player = $AudioStreamPlayer2D
-
 var new_stream: AudioStream
 
 signal change_level(level)
 signal main_menu
 
-
-
 func _on_change_level(level):
-	new_stream = load(MUSIC_PATH + "level" + str(level) +".mp3")
-	player.stream = new_stream
-	player.play()
+	play_file("level" + str(level))
 
 func _on_main_menu():
-	new_stream = preload(MUSIC_PATH + "time_for_adventure.mp3")
+	play_file("time_for_adventure")
+	
+func play_file(file):
+	new_stream = load(MUSIC_PATH + str(file) +".mp3")
 	player.stream = new_stream
 	player.play()
