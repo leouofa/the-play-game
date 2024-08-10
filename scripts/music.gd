9a015d3605e126547a665633d86f27806c00891b
Leonid Medovyy <leonid@macron1.io>
2024-08-10 13:45:02 -0700

    setting up per level music files

--- scripts/music.gd
+++ scripts/music.gd
@@ -1,13 +1,22 @@
 extends Node
 
+const MUSIC_PATH = "res://assets/music/"
+
 @onready var player = $AudioStreamPlayer2D
+
+var new_stream: AudioStream
 
 signal change_level(level)
 signal main_menu
 
 
+
 func _on_change_level(level):
-	print("Changing Level: " + str(level))
+	new_stream = load(MUSIC_PATH + "level" + str(level) +".mp3")
+	player.stream = new_stream
+	player.play()
 
 func _on_main_menu():
-	print("Changing To Main Menu")
+	new_stream = preload(MUSIC_PATH + "time_for_adventure.mp3")
+	player.stream = new_stream
+	player.play()
