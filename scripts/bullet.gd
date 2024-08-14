f9838b700e19034db0cc0ffecd0521d8f80a7f69
Leonid Medovyy <leonid@macron1.io>
2024-08-13 20:52:02 -0700

    added ability for slime to get shot

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 const SPEED = 300
+const DAMAGE = 100
+
 var direction = Vector2.RIGHT
 
 func _ready():
@@ -11,3 +13,6 @@ func _process(delta):
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
+
+func bullet_deal_damage():
+	return DAMAGE
