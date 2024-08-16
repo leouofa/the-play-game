0d3c636bcb0d3ea92a6028e5d5f0e9f2e97f770f
Leonid Medovyy <leonid@macron1.io>
2024-08-16 15:41:24 -0700

    adding particles to shooting guns

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,11 +3,17 @@ extends Area2D
 const SPEED = 400
 const DAMAGE = 100
 @onready var animation_player = $AnimationPlayer
+@onready var particles = $GPUParticles2D
 
 var direction = Vector2.RIGHT
 
 func _ready():
 	animation_player.play("pulse")
+
+	if direction == Vector2.LEFT:
+		particles.process_material.set("gravity", Vector3(-400, 0, 0))
+	else:
+		particles.process_material.set("gravity", Vector3(400, 0, 0))
 
 func _process(delta):
 	position += (direction * SPEED) * delta
