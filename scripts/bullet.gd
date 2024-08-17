717c1d5fd954d48c8c8482f470a96eb4c090866d
Leonid Medovyy <leonid@macron1.io>
2024-08-17 09:57:33 -0700

    adding collision particles

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -6,6 +6,9 @@ const DAMAGE = 100
 @onready var particles = $GPUParticles2D
 
 var direction = Vector2.RIGHT
+var hit = preload("res://scenes/hit.tscn")
+
+@onready var marker_2d = $Marker2D
 
 func _ready():
 	animation_player.play("pulse")
@@ -26,3 +29,10 @@ func bullet_deal_damage():
 
 func remove_bullet():
 	queue_free()
+
+func _on_area_entered(area):
+	print("collided with bullet")
+	print(area)
+	var hit_instance = hit.instantiate()
+	get_parent().add_child(hit_instance)
+	hit_instance.global_position = marker_2d.global_position
